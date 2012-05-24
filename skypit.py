49b4a950e868fcf0275797daf9c5083cf8267fa7
Eikichi <megane@glasses-factory.net>
2012-05-24 15:18:25 +0900

    ぬー

--- skypit.py
+++ skypit.py
@@ -3,21 +3,24 @@
 import Skype4Py
 import time
 import sys
+import logging
 
-from flask import Flask,
+from flask import Flask, Request, Response
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
 
 class Skypit(object):
 	def __init__(self,room):
-		self.sp = Skype4Py.Skype(Transport='x11')
+		# self.sp = Skype4Py.Skype(Transport='x11')
+		self.sp = Skype4Py.Skype()
 		self.sp.Attach()
 		self.chat = self.selectChatFromBookmark(self.sp,room)
 
 	def selectChatFromBookmark(self,skype,room):
-		for bookmarked in skype.BookMarkedChats:
-			if bookmarked.Topic == topic:
-				return chat
+		for bookmarked in skype.BookmarkedChats:
+			if bookmarked.Topic == room:
+				return bookmarked
 
 	def notification(self,msg):
 		self.chat.SendMessage(msg)
@@ -28,13 +31,30 @@ class Skypit(object):
 
 pit = None
 
-@app.rout('/<path:name>')
-def show(name):
-	msg = u'リポジトリ' + name + u'に push されました。'
-	pit.notification(msg)
+@app.route('/')
+def index():
+	return u'はろー'
 
-if __name__ == '__main__':
-	roomName = uicode(sys.args[1], 'utf-8')
+@app.route('/<name>')
+def show(name):
+	msg = u'[github]::リポジトリ %s に push されました。' % name
+	pit.notification(msg)
+	return msg
+
+@app.before_first_request
+def setUpSkype():
+	# roomName = unicode(sys.argv[1], 'utf-8')
+	roomName = 'test'
 	pit = Skypit(roomName)
 	pit.run()
+
+from werkzeug.contrib.fixers import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+
+
+if __name__ == '__main__':
+	roomName = unicode(sys.argv[1], 'utf-8')
+	pit = Skypit(roomName)
 	app.run()
+	pit.run()
